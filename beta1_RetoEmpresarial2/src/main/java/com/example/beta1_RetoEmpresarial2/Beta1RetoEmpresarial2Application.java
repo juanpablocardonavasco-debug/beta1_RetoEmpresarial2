2ad89bcc89fb523bf5bff57d198b4bcfdd9d64da
Juan Pablo Cardona Vasco <juanpablocardonavasco@gmail.com>
2026-09-18 06:24:10 -0500

    feat se crea proyecto

---
+++ beta1_RetoEmpresarial2/src/main/java/com/example/beta1_RetoEmpresarial2/Beta1RetoEmpresarial2Application.java
@@ -0,0 +1,13 @@
+package com.example.beta1_RetoEmpresarial2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Beta1RetoEmpresarial2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Beta1RetoEmpresarial2Application.class, args);
+	}
+
+}
---
+++ beta1_RetoEmpresarial2/src/test/java/com/example/beta1_RetoEmpresarial2/Beta1RetoEmpresarial2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beta1_RetoEmpresarial2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Beta1RetoEmpresarial2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
