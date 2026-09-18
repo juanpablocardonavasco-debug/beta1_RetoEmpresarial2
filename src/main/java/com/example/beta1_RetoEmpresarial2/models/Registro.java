7782a1a9cbddb93815d8464559d327e8f2b3be9a
Juan Pablo Cardona Vasco <juanpablocardonavasco@gmail.com>
2026-09-18 08:34:03 -0500

    se crea modelo registro

--- src/main/java/com/example/beta1_RetoEmpresarial2/models/Registro.java
+++ src/main/java/com/example/beta1_RetoEmpresarial2/models/Registro.java
@@ -1,5 +1,78 @@
 package com.example.beta1_RetoEmpresarial2.models;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "registros")    
 public class Registro {
+@Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 
+    @Column(name = "fecha_registro", nullable = false, unique = false)
+    private LocalDateTime fechaRegistro;
+
+    @Column(name = "observacion", nullable = true, unique = false, length = 255)
+    private String observacion;
+
+    @Column(name = "estado", nullable = false, unique = false, length = 30)
+    private String estado;
+
+    @ManyToOne
+    @JoinColumn(name = "id_reto", nullable = false)
+    @JsonBackReference
+    private Reto reto;
+
+    public Registro() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(LocalDateTime fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public String getObservacion() {
+        return observacion;
+    }
+
+    public void setObservacion(String observacion) {
+        this.observacion = observacion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Reto getReto() {
+        return reto;
+    }
+
+    public void setReto(Reto reto) {
+        this.reto = reto;
+    }
 }
