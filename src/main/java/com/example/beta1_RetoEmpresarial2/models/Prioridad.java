8ca378b2852b78aabba491a5ad9b4169ffd4eb84
Mattsito89 <135280140+Mattsito89@users.noreply.github.com>
2026-09-25 06:34:12 -0500

    Se sube prioridad

---
+++ src/main/java/com/example/beta1_RetoEmpresarial2/models/Prioridad.java
@@ -0,0 +1,68 @@
+package com.example.RetoEmpresarial.modelos;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "prioridades")
+public class Prioridad {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "nombre", nullable = false, unique = false, length = 50)
+    private String nombre;
+
+    @Column(name = "nivel", nullable = false, unique = false)
+    private Integer nivel;
+
+    @OneToMany(mappedBy = "prioridad")
+    @JsonManagedReference
+    private List<Reto> retos = new ArrayList<>();
+
+    public Prioridad() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(Integer nivel) {
+        this.nivel = nivel;
+    }
+
+    public List<Reto> getRetos() {
+        return retos;
+    }
+
+    public void setRetos(List<Reto> retos) {
+        this.retos = retos;
+    }
+}
